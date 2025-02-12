b2e4e026fb24e5051b7562a27af7b4bedc265a58
AsadAnalyst <syed0055asad0@gmail.com>
2025-02-12 15:30:40 +0500

    Safe Home (Lab-2 Task-3)

--- Car Maintenance Check (L-3_T-3).py
+++ Car Maintenance Check (L-3_T-3).py
@@ -1,30 +1,28 @@
-maintenance = {
-    "Engine noise": 4,
-    "Check engine light": 5,
-    "Poor fuel efficiency": 3,
-    "Strange vibrations": 3,
-    "Difficulty starting": 4,
-    "Braking issues": 5,
-    "Unusual exhaust smoke": 4,
-    "Steering problems": 3
+SafeHome = {
+    "Exposed electrical wiring": 5,
+    "Gas leak smell": 5,
+    "Water leakage or mold growth": 4,
+    "Loose stair railings": 3,
+    "Faulty smoke detectors": 4,
+    "Clogged or blocked exits": 3,
+    "Slippery floors without mats": 3,
+    "Windows or doors that don’t lock properly": 4
 }
 
-total_score = 0
+totalScore = 0
 
-for issue in maintenance:
-    print("Are you experiencing", issue, "? (Yes = 1, No = 0)")
+for test in SafeHome:
+    print("Are you experiencing", test, "(Yes = 1, No = 0)")
     while True:
-        user_input = input()
-        if user_input == "1" or user_input == "0":
+        responce = input()
+        if responce == "1" or responce == "0":
             break
-        print("Please enter 1 for Yes or 0 for No.")
+        print("Please Enter (Yes = 1, No = 0)")
 
-    if user_input == "1":
-        total_score = total_score + maintenance[issue]
+    if responce == "1":
+        totalScore = totalScore + SafeHome[test]
 
-threshold = 15
-
-if total_score > threshold:
-    print("Based on the issues reported, your car may require maintenance. It is recommended to visit a mechanic for a thorough inspection.")
+if totalScore > 15:
+    print("Based on the hazards reported, your home may require safety improvements. It is recommended to address these issues to prevent accidents")
 else:
-    print("Based on the issues reported, your car does not seem to require immediate maintenance. However, if you are concerned, consulting a mechanic is always a good idea.")
+    print("Based on the hazards reported, your home does not seem to have critical safety risks. However, regular maintenance is always a good idea.")
